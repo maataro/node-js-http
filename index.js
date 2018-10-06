32239f093f5b3b308fd8a183cb38062cbc5a8da6
maataro <masahiro.infinite77@gmail.com>
2018-10-06 17:19:57 +0000

    アクセスログ取得のコードを追加

--- index.js
+++ index.js
@@ -2,16 +2,15 @@
 
 const http = require('http');
 const server = http.createServer((req, res) => {
+    console.info('[' + new Date() + '] Requested by ' + req.connection.remoteAddress);
     res.writeHead(200, {
-        'Content-Type': 'text/plain; charset=utf-8'
-        
+        'Content-Type': 'text/plain; charset=utf-8'        
     });
     res.write(req.headers['user-agent']);
     res.write('こんにちは！！')
     res.end();
 });
 const port = 8000;
-server.listen(port, () => {
-    // console.log('Listening on ' + port);
+server.listen(port, () => {    
     console.log(`Listening on ${port}`);
 });
